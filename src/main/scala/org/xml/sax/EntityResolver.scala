36b729d69ec4a249dbaaf6bcf295f83ef27b0841
Erik Bruchez <erik@bruchez.org>
2020-12-26 20:24:59 -0800

    Fix method's return type

--- src/main/scala/org/xml/sax/EntityResolver.scala
+++ src/main/scala/org/xml/sax/EntityResolver.scala
@@ -2,8 +2,7 @@ package org.xml.sax
 
 import java.io.IOException
 
+
 trait EntityResolver {
-
-
-  def resolveEntity(publicId: String, systemId: String): Unit
+  def resolveEntity(publicId: String, systemId: String): InputSource
 }
